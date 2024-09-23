936f1b1a71e99c7b4fc0a2b23abb85b3dc48f931
Harsh Kumar <phmahawars@gmai.com>
2024-09-23 13:56:11 +0530

    wsws

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
 <body>
     <h1>Video Streaming Example</h1>
     <video controls width="600">
-        <source src="video.php" type="video/mp4">
+        <source src="video.php" type="video/mp4" codecs="avc1.4d002a, mp4a.40.2">
         Your browser does not support the video tag.
     </video>
     <video controls width="600">
-        <source src="video/earth_in_4k_by_onehome.mp4" type="video/mp4">
+        <source src="video/manjummel-boys.mp4" type="video/mp4" codecs="avc1.4d002a, mp4a.40.2">
         Your browser does not support the video tag.
     </video>
 </body>
